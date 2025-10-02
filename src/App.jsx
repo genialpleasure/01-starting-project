30fca2ed7f42227a70d8f5b97f02e48499f0ed52
genialpleasure <isaacnpleasure01@gmail.com>
2025-10-02 23:36:36 +0100

    Add new components and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './Data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return <li>
-    <img scr={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-}
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -38,22 +12,32 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-            title={CORE_CONCEPTS[0].title} 
-            description={CORE_CONCEPTS[0].description} 
-            image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept
-            title={CORE_CONCEPTS[1].title} 
-            description={CORE_CONCEPTS[1].description} 
-            image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
             />
             <CoreConcept
-            title={CORE_CONCEPTS[2].title} 
-            description={CORE_CONCEPTS[2].description} 
-            image={CORE_CONCEPTS[2].image}
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
             />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
